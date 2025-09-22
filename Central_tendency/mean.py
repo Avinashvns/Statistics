7d6b1856111e53f9b86d13274d6d11c88c51ea5c
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-09-22 15:16:02 +0530

    Median
    Finding Even and odd

--- Central_tendency/mean.py
+++ Central_tendency/mean.py
@@ -1,8 +1,15 @@
 
 import numpy as np
+
+# Mean of the age value
 age= [28,56,35,28,76,89]
-
-length=len(age)
-print(length)
-
 print(np.mean(age))
+
+
+# Median of the even classes
+classes_even=[5,4,9,2,8,6]
+print(np.median(classes_even))
+
+# Median of the even classes
+classes_odd=[5,4,9,2,8,6,10]
+print(np.median(classes_odd))
